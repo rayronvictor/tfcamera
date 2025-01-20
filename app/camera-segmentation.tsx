7754b748903c11b6358f045573571aa338b2d79f
Rayron Victor <rayronaraujo@tjrn.jus.br>
2025-01-20 14:26:50 -0300

    wip

--- app/camera-segmentation.tsx
+++ app/camera-segmentation.tsx
@@ -1,47 +1,79 @@
-import {Platform, StyleSheet} from 'react-native';
+import {StyleSheet} from 'react-native';
 import {
   Camera,
   runAtTargetFps,
   useCameraDevice,
   useCameraPermission,
-  useSkiaFrameProcessor,
 } from "react-native-vision-camera";
 import {useResizePlugin} from "vision-camera-resize-plugin";
-import {matchFont, PaintStyle, Skia} from "@shopify/react-native-skia";
+import {Canvas, createPicture, PaintStyle, Picture, Skia, SkSize} from "@shopify/react-native-skia";
+import {useNavigation} from "expo-router";
 
 import {NoCameraDeviceError, PermissionRequest} from "@/components/PermissionRequest";
 import {useTensorflowModel} from "react-native-fast-tflite";
+import {useDerivedValue, useSharedValue} from "react-native-reanimated";
+import {Worklets} from "react-native-worklets-core";
+import {useCameraFormat, useFrameProcessor} from "react-native-vision-camera/src";
+import {useIsForeground} from "@/hooks/useIsForeground";
 
-const fontFamily = Platform.select({ ios: 'Helvetica', default: 'serif' });
-const font = matchFont({
-  fontFamily,
-  fontSize: 12,
-  fontStyle: "italic",
-  fontWeight: 'bold',
-});
+type Box = [number, number, number, number]
 
 const paint = Skia.Paint()
 paint.setStyle(PaintStyle.Stroke)
 paint.setStrokeWidth(4);
 
+const IMAGE_ASPECT_RATIO = 16 / 9;
 const COLORS = ['red', 'blue', 'green', 'yellow', 'purple'];
 
 export default function CameraSegmentationScreen() {
+  const navigation = useNavigation();
   const {hasPermission} = useCameraPermission()
 
   const device = useCameraDevice("back");
+  const format = useCameraFormat(device, [
+    { fps: 30 },
+    { videoAspectRatio: IMAGE_ASPECT_RATIO },
+  ]);
+
+  const isActive = useIsForeground() && navigation.isFocused();
 
   const { resize } = useResizePlugin()
   const { model, state } = useTensorflowModel(require('../assets/models/yolov11_seg.tflite'))
 
+  const canvasSize = useSharedValue<SkSize>({ width: 0, height: 0 });
+  const offsetX = useSharedValue(0);
+  const offsetY = useSharedValue(0);
 
-  const frameProcessor = useSkiaFrameProcessor((frame) => {
+  const videoCanvasRatio = useSharedValue(0);
+
+  const boxes = useSharedValue<Box[]>([]);
+  const updateBoxes = Worklets.createRunOnJS((value: Box[]) => {
+    boxes.value = value;
+  })
+
+  const frameProcessor = useFrameProcessor((frame) => {
     'worklet'
-    frame.render();
 
     if (state !== 'loaded') {
       // model is still loading or with error...
       return
+    }
+
+    console.log(`frame: ${frame.width} x ${frame.height}`);
+
+    const IMAGE_SIZE = 640;
+
+    const ratio = IMAGE_SIZE / frame.width;
+    const width = frame.width * ratio;
+    const height = frame.height * ratio;
+
+    let offsetX = 0;
+    let offsetY = 0;
+
+    if (width > height) {
+      offsetX = (width - height) / 2;
+    } else {
+      offsetY = (height - width) / 2;
     }
 
     // runAsync(frame, () => {
@@ -51,56 +83,100 @@ export default function CameraSegmentationScreen() {
 
       const resized = resize(frame, {
         scale: {
-          width: 640,
-          height: 640,
+          width: IMAGE_SIZE,
+          height: IMAGE_SIZE,
         },
         pixelFormat: 'rgb',
         dataType: 'float32', // YOLOv11 uses float32
+        // rotation: "270deg", // TODO test if this is correct
+        rotation: "90deg", // TODO test if this is correct
       })
 
       const [boxes, scores, masks, class_idx, protos] = model?.runSync([resized])
 
       const scoreIdxs = filterByConfidence(scores, 0.5)
 
-      console.log(JSON.stringify(scoreIdxs, null, 2))
-
-      // const maxScoreIdx = scores.reduce((maxScoreIdx: number, current: number, idx: number, arr: Float32Array<ArrayBufferLike>) => {
-      //   return current > arr[maxScoreIdx] ? idx : maxScoreIdx;
-      // }, 0)
-
-
+      let newBoxes: Box[] = []
 
       for(let i = 0; i < scoreIdxs?.length; i++) {
         // multiply the index by 4 (xc, yc, width, height => 4 values)
         const boxPos = scoreIdxs[i] * 4
-        const xc = Number(boxes[boxPos])
-        const yc = Number(boxes[boxPos + 1])
-        const width = Number(boxes[boxPos + 2])
-        const height = Number(boxes[boxPos + 3])
+        const xc = (Number(boxes[boxPos]) + offsetX) / ratio;
+        const yc = (Number(boxes[boxPos + 1]) + offsetY) / ratio;
+        const width = Number(boxes[boxPos + 2]) / ratio;
+        const height = Number(boxes[boxPos + 3]) / ratio;
 
-        console.log(`Idx: ${i}, score: ${scores[scoreIdxs[i]]}, class: ${class_idx[scoreIdxs[i]]}`)
-        console.log(JSON.stringify(`Box: { xc: ${xc}, yc: ${yc}, w: ${width}, h: ${height} }`, null, 2));
+        // console.log(`Idx: ${i}, score: ${scores[scoreIdxs[i]]}, class: ${class_idx[scoreIdxs[i]]}`)
+        // console.log(JSON.stringify(`Box: { xc: ${xc}, yc: ${yc}, w: ${width}, h: ${height} }`, null, 2));
 
-        paint.setColor(Skia.Color(COLORS[i]))
+        console.log(JSON.stringify(`offsetX: ${offsetX}, offsetY: ${offsetY}, ratio: ${ratio}`, null, 2));
 
-        const rect = Skia.XYWHRect(xc - width / 2, yc - height / 2, width, height);
-        frame.drawRect(rect, paint);
+        newBoxes.push([xc - width / 2, yc - height / 2, width, height])
+      }
+
+      updateBoxes(newBoxes);
+    })
+  }, [state, model])
+
+  useDerivedValue(() => {
+    const { width, height } = canvasSize.value
+
+    if (format) {
+      videoCanvasRatio.value = width / format.videoWidth;
+    }
+
+    if (width > height) {
+      offsetX.value = (width - (height * IMAGE_ASPECT_RATIO)) / 2;
+      offsetY.value = 0;
+    } else {
+      offsetX.value = 0;
+      offsetY.value = (height - (width * IMAGE_ASPECT_RATIO)) / 2;
+    }
+  }, [format])
+
+  const pictures = useDerivedValue(() => {
+    return createPicture((canvas) => {
+      for(let i = 0; i < boxes.value.length; i++) {
+        const [xc, yc, width, height] = boxes.value[i];
+
+        paint.setColor(Skia.Color(COLORS[i % COLORS.length]))
+
+        const rect = Skia.XYWHRect(
+          xc * videoCanvasRatio.value,
+          yc * videoCanvasRatio.value,
+          width * videoCanvasRatio.value,
+          height * videoCanvasRatio.value,
+        );
+
+        console.log(`Rect: { x: ${rect.x}, y: ${rect.y}, width: ${rect.width}, height: ${rect.height}`);
+
+        canvas.drawRect(rect, paint);
       }
     })
-
-  }, [state, model])
+  })
 
   if (!hasPermission) return <PermissionRequest />
   if (device == null) return <NoCameraDeviceError />
 
   return (
-    <Camera
-      enableFpsGraph={__DEV__}
-      style={StyleSheet.absoluteFill}
-      device={device}
-      isActive={true}
-      frameProcessor={frameProcessor}
-    />
+    <>
+      <Camera
+        device={device}
+        format={format}
+        isActive={isActive}
+        frameProcessor={frameProcessor}
+        resizeMode="contain"
+        enableFpsGraph={__DEV__}
+        style={StyleSheet.absoluteFill}
+      />
+
+      <Canvas
+        style={StyleSheet.absoluteFill}
+        onSize={canvasSize}
+      >
+        <Picture picture={pictures} />
+      </Canvas>
+    </>
   );
 }
 
